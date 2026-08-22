56a4a1297a00c779d4cefe015c7b6524005f0cba
pavelpavelkiselev <paalkiselev@edu.hse.ru>
2026-08-22 22:26:54 +0300

    Predictor update.

--- src/predictor.hpp
+++ src/predictor.hpp
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include <array>
 #include <cmath>
 #include <cstdint>
 #include <vector>
@@ -23,28 +24,22 @@ private:
   uint8_t prev1 = 0;
   uint16_t prev2 = 0;
 
+  std::array<std::array<uint32_t, 2>, 256> count0 = {};
+  std::vector<std::array<std::array<uint32_t, 2>, 256>> count1 = 
+    std::vector<std::array<std::array<uint32_t, 2>, 256>>(256);
+  std::vector<std::array<std::array<uint32_t, 2>, 256>> count2 = 
+    std::vector<std::array<std::array<uint32_t, 2>, 256>>(65536);
+
   long double w0 = 1.0;
   long double w1 = 1.0;
   long double w2 = 1.0;
-
-  uint64_t count0[256][2] = {0};
-
-  std::vector<std::vector<std::vector<uint64_t>>> count1 =
-      std::vector<std::vector<std::vector<uint64_t>>>(
-          256, std::vector<std::vector<uint64_t>>(
-                   256, std::vector<uint64_t>(2, 0)));
-
-  std::vector<std::vector<std::vector<uint64_t>>> count2 =
-      std::vector<std::vector<std::vector<uint64_t>>>(
-          65536, std::vector<std::vector<uint64_t>>(
-                     256, std::vector<uint64_t>(2, 0)));
 
   /**
    * @brief Converts a probability to logistic space (log-odds).
    * @param p Probability value in range (0, 1).
    * @return Log-odds representation.
    */
-  inline long double stretch(long double p) const {
+  inline double stretch(const double p) const {
     return std::log(p / (1.0 - p));
   }
 
@@ -53,8 +48,8 @@ private:
    * @param x Logistic value.
    * @return Probability in range (0, 1).
    */
-  inline long double squash(long double x) const {
-    return 1.0 / (1.0 + std::exp(-x));
+  inline long double squash(const long double x) const {
+    return (1.0 / (1.0 + std::exp(-x)));
   }
 
   /**
@@ -62,7 +57,7 @@ private:
    * @param p Original probability.
    * @return Clamped probability.
    */
-  inline long double clamp_p(long double p) const {
+  inline long double clamp_p(const long double p) const {
     if (p < 0.00005) {
       return 0.00005;
     } else if (p > 0.99995) {
@@ -72,17 +67,32 @@ private:
     }
   }
 
+  /**
+   * @brief Clamps the weight to avoid -inf to inf range.
+   * @param w Original weight.
+   * @return Clamped weight.
+   */
+  inline long double clamp_w(const long double w) const {
+    if (w < -10.0L) {
+      return -10.0L;
+    }
+    if (w > 10.0L) {
+      return 10.0L;
+    }
+    return w;
+  };
+
 public:
   /**
    * @brief Predicts the probability that the next bit will be 0.
-   * @return A scaled integer probability at [0; 2^64).
+   * @return A scaled integer probability at [0; 2^32).
    */
-  uint64_t next_bit_probability() const;
+  uint32_t next_bit_probability() const;
 
   /**
    * @brief Updates the context models and neural network weights.
    * @param bit The actual bit that was exctracted.
    */
-  void update_model(bool bit);
+  void update_model(const bool bit);
 };
 
