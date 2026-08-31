4f42486c4f1fb523582dc18281ae4d6010cc89f5
pavelpavelkiselev <paalkiselev@edu.hse.ru>
2026-08-31 16:23:05 +0300

    Encoder updated to predictor's 16 bit arithmetic.

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -8,10 +8,10 @@
 Encoder::Encoder(std::ifstream &in, std::ofstream &out) : input(in), output(out) {}
 
 void Encoder::encode(const bool bit) {
-    const uint64_t range = static_cast<uint64_t>(high - low);
-    const uint64_t prob = static_cast<uint64_t>(predictor.next_bit_probability());
-    const uint64_t result = (range * prob) >> 32;
-    const uint32_t mid = low + static_cast<uint32_t>(result);
+    const uint32_t range = static_cast<uint32_t>(high - low);
+    const uint32_t prob = static_cast<uint32_t>(predictor.next_bit_probability());
+    const uint32_t result = (range * prob) >> 16;
+    const uint16_t mid = low + static_cast<uint16_t>(result);
 
     if (bit) {
         low = mid + 1;
@@ -21,20 +21,20 @@ void Encoder::encode(const bool bit) {
 
     predictor.update_model(bit);
 
-    while (((low ^ high) >> 24) == 0) {
+    while (((low ^ high) >> 8) == 0) {
         /*  While the highest byte of low and high is the same,
             flush the byte to the file. */
-        output.put(static_cast<unsigned char>(high >> 24));
+        output.put(static_cast<unsigned char>(high >> 8));
         low <<= 8;
         high = (high << 8) | 0xFF;
     }
 }
 
 bool Encoder::decode() {
-    const uint64_t range = static_cast<uint64_t>(high - low);
-    const uint64_t prob = static_cast<uint64_t>(predictor.next_bit_probability());
-    const uint64_t result = (range * prob) >> 32;
-    const uint32_t mid = low + static_cast<uint32_t>(result);
+    const uint32_t range = static_cast<uint32_t>(high - low);
+    const uint32_t prob = static_cast<uint32_t>(predictor.next_bit_probability());
+    const uint32_t result = (range * prob) >> 16;
+    const uint16_t mid = low + static_cast<uint16_t>(result);
     const bool bit = (current > mid);
 
     if (bit) {
@@ -45,7 +45,7 @@ bool Encoder::decode() {
 
     predictor.update_model(bit);
 
-    while (((low ^ high) >> 24) == 0) {
+    while (((low ^ high) >> 8) == 0) {
         low <<= 8;
         high = (high << 8) | 0xFF;
         int c = input.get();
